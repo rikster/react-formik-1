c66e2d494fdc38222bb325b16babbdbe3b0277a2
Richard Hounslow <richard.hounslow@sportsbet.com.au>
2021-10-19 14:02:33 +1100

    optimise w/ Field an Errormessage components (abstracts manual checking)

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,79 +1,55 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import {ErrorMessage, Field, Form, Formik} from 'formik'
 import * as Yup from 'yup'
 
 const initialValues = {
-  name: 'Vishwas',
-  email: '',
-  channel: ''
+    name: 'Vishwas',
+    email: '',
+    channel: ''
 }
 
 const onSubmit = values => {
-  console.log('Form data', values)
+    console.log('Form data', values)
 }
 
 const validationSchema = Yup.object({
-  name: Yup.string().required('Required'),
-  email: Yup.string()
-    .email('Invalid email format')
-    .required('Required'),
-  channel: Yup.string().required('Required')
+    name: Yup.string().required('Required'),
+    email: Yup.string()
+        .email('Invalid email format')
+        .required('Required'),
+    channel: Yup.string().required('Required')
 })
 
-function YoutubeForm () {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    // validate,
-    validationSchema
-  })
+function YoutubeForm() {
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <Form>
+                <div className='form-control'>
+                    <label htmlFor='name'>Name</label>
+                    <Field type='text' id='name' name='name'/>
+                    <ErrorMessage name='name'/>
+                </div>
 
-  console.log('formik.touched', formik.touched)
+                <div className='form-control'>
+                    <label htmlFor='email'>E-mail</label>
+                    <Field type='email' id='email' name='email'/>
+                    <ErrorMessage name='email'/>
+                </div>
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className='form-control'>
-        <label htmlFor='name'>Name</label>
-        <input
-          type='text'
-          id='name'
-          name='name'
-          {...formik.getFieldProps('name')}
-        />
-        {formik.touched.name && formik.errors.name ? (
-          <div className='error'>{formik.errors.name}</div>
-        ) : null}
-      </div>
+                <div className='form-control'>
+                    <label htmlFor='channel'>Channel</label>
+                    <Field type='text' id='channel' name='channel'/>
+                    <ErrorMessage name='channel'/>
+                </div>
 
-      <div className='form-control'>
-        <label htmlFor='email'>E-mail</label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          {...formik.getFieldProps('email')}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <div className='error'>{formik.errors.email}</div>
-        ) : null}
-      </div>
-
-      <div className='form-control'>
-        <label htmlFor='channel'>Channel</label>
-        <input
-          type='text'
-          id='channel'
-          name='channel'
-          {...formik.getFieldProps('channel')}
-        />
-        {formik.touched.channel && formik.errors.channel ? (
-          <div className='error'>{formik.errors.channel}</div>
-        ) : null}
-      </div>
-
-      <button>Submit</button>
-    </form>
-  )
+                <button type='submit'>Submit</button>
+            </Form>
+        </Formik>
+    )
 }
 
 export default YoutubeForm
